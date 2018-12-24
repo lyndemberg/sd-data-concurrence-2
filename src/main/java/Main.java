31c7f578916b6d18b4eab0457f8ab753f6ec81b5
lyndemberg <lyndembergbatista@outlook.com>
2018-12-24 19:51:49 -0300

    pausando e reiniciando

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
+import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     private static String INSTANCE_APP;
     private static String PATH_FILE_LOCK_SHARED;
     private static int CAPACITY_QUEUES;
+
+    private static Lock lockExecucao = new ReentrantLock();
+    private static Condition condicaoExecucao = lockExecucao.newCondition();
+    private static boolean seguir = true;
+    private static int opcaoExecucao = 0;
 
     public static void main(String[] args) throws InterruptedException, IOException {
         INSTANCE_APP = args[0];
@@ -20,6 +30,28 @@ public class Main {
         RandomAccessFile raf = new RandomAccessFile(fileLock, "rw");
         FileChannel channel = raf.getChannel();
 
+        Runnable runnable = new Runnable() {
+            private Scanner sc = new Scanner(System.in);
+            public void run() {
+                System.out.println("Digite: ");
+                System.out.println("1- PAUSAR");
+                System.out.println("2- CONTINUAR");
+                while(true){
+                    opcaoExecucao = sc.nextInt();
+                    System.out.println("VOCÊ ESCOLHEU "+ opcaoExecucao);
+                    lockExecucao.lock();
+                        if(opcaoExecucao == 1) {
+                            seguir = false;
+                        }else if(opcaoExecucao == 2) {
+                            seguir = true;
+                            condicaoExecucao.signalAll();
+                        }
+                    lockExecucao.unlock();
+                }
+            }
+        };
+        new Thread(runnable).start();
+
         final UserDao userDao = new UserDao();
 
         int contador = 1;
@@ -28,77 +60,84 @@ public class Main {
         final BlockingQueue<Integer> queueInsert = new ArrayBlockingQueue<Integer>(CAPACITY_QUEUES);
         final BlockingQueue<Integer> queueUpdate = new ArrayBlockingQueue<Integer>(CAPACITY_QUEUES);
         final BlockingQueue<Integer> queueDelete = new ArrayBlockingQueue<Integer>(CAPACITY_QUEUES);
+
         while(contador <= limite){
-            //LOCK DATABASE BETWEEN FILE LOCK SHARED
+            // ENQUANTO LOCK TIVER BLOQUEADO NÃO EXECUTA, ESPERA O USUÁRIO NOTIFICAR PARA CONTINUAR
+            lockExecucao.lock();
+                while(!seguir){
+                    condicaoExecucao.await();
+                }
+            lockExecucao.unlock();
+                //LOCK DATABASE BETWEEN FILE LOCK SHARED
                 FileLock lock = channel.lock();
                 int lastIdLocked = userDao.getLastIdLocked();
-                if(lastIdLocked == limite){
+                if (lastIdLocked == limite) {
                     //JÁ HOUVE RESERVAS ATÉ O LIMITE
                     lock.release();
                     imprimirTempo(t0);
                     break;
-                }else{
+                } else {
                     //RESERVAR NOVO ID
                     contador = lastIdLocked + 1;
                     userDao.lockId(contador, INSTANCE_APP);
                     lock.release();
                 }
-            //UNLOCK DATABASE
+                //UNLOCK DATABASE
 
-            queueInsert.put(contador);
+                queueInsert.put(contador);
 
-            Runnable runnableInsert = new Runnable() {
-                public void run() {
-                    try {
-                        Integer take = queueInsert.take();
-                        User user = new User(take,"JoãoDeDeus");
-                        userDao.insert(user);
-                        queueUpdate.put(take);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-
-            Runnable runnableUpdate = new Runnable() {
-                public void run() {
-                    try {
-                        Integer take = queueUpdate.take();
-                        userDao.update(take);
-                        queueDelete.put(take);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-
-            Runnable runnableDelete = new Runnable() {
-                public void run() {
-
-                    try {
-                        Integer take = queueDelete.take();
-                        userDao.delete(take);
-                        if (take == limite) {
-                            imprimirTempo(t0);
+                Runnable runnableInsert = new Runnable() {
+                    public void run() {
+                        try {
+                            Integer take = queueInsert.take();
+                            User user = new User(take, "JoãoDeDeus");
+                            userDao.insert(user);
+                            queueUpdate.put(take);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
                         }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
                     }
-                }
-            };
+                };
 
-            Thread threadInsert = new Thread(runnableInsert);
-            Thread threadUpdate = new Thread(runnableUpdate);
-            Thread threadDelete = new Thread(runnableDelete);
-            threadInsert.start();
-            threadUpdate.start();
-            threadDelete.start();
+                Runnable runnableUpdate = new Runnable() {
+                    public void run() {
+                        try {
+                            Integer take = queueUpdate.take();
+                            userDao.update(take);
+                            queueDelete.put(take);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
 
-            contador ++;
+                Runnable runnableDelete = new Runnable() {
+                    public void run() {
 
-        }
+                        try {
+                            Integer take = queueDelete.take();
+                            userDao.delete(take);
+                            if (take == limite) {
+                                imprimirTempo(t0);
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                Thread threadInsert = new Thread(runnableInsert);
+                Thread threadUpdate = new Thread(runnableUpdate);
+                Thread threadDelete = new Thread(runnableDelete);
+                threadInsert.start();
+                threadUpdate.start();
+                threadDelete.start();
+
+                contador++;
+
+            }
+
     }
-
     private static void imprimirTempo(long inicio){
         long t1 = System.currentTimeMillis();
         long tempoTotal = t1 - inicio;
